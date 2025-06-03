b73678388020fff5d1841bdb8e392e0ff712aee1
Johannes Pfau <johannes@pfau.cc>
2025-06-04 00:08:17 +0200

    finished functionality, updated templates and made it responsive

--- functions.php
+++ functions.php
@@ -11,3 +11,10 @@ include(get_theme_file_path('/includes/setup.php'));
 // hooks
 add_action('wp_enqueue_scripts', 'fblgst_enqueue');
 add_action('after_setup_theme', 'fblgst_setup_theme');
+
+
+// filter
+function fblgst_custom_excerpt_length( $length ) {
+	return 20;
+}
+add_filter( 'excerpt_length', 'fblgst_custom_excerpt_length', 999 );
